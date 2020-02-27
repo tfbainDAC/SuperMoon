94e8fecd894d9acdfaee875f2f84eddd17a36c54
t.bain <t.bain@AC005X08.ad.dundeeandangus.ac.uk>
2020-02-27 11:17:41 +0000

    added comment to customer class

---
+++ src/moon2/Customer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moon2;
+
+/**
+ *
+ * @author t.bain
+ */
+public class Customer {
+    /* demonstrating addition of new class  */
+   private int _custID;
+   private String _fName;
+   private String _surName;
+   private String _address;
+   private Payment[] _paymentCard;
+   
+   public static void hello(){
+       System.out.println("hello");
+   }
+	
+}
